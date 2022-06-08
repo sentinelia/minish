5af82568f8eae0cde5afa8e802bc57cbb9583374
lucasjmelogno-montevideolabs <81168112+sentinelia@users.noreply.github.com>
2022-06-08 15:26:26 -0300

    Estructura

--- externo.c
+++ externo.c
@@ -9,47 +9,45 @@
 #include <signal.h>
 #include "minish.h"
 
-
-
 int externo(int argc, char  **argv){
     pid_t pid;                          // process ID: an unsigned integer type
-    int wait_status;                    // wait status will be filled by waitpid syscall
-    struct sigaction oldact, newact;
+            int wait_status;                    // wait status will be filled by waitpid syscall
 
-    sigaction(SIGINT, NULL, &oldact);   // the  previous action for SIGINT is saved in oldact
-    newact = oldact;
+            fprintf(stderr, "Will fork sleep command\n");
 
-    if ((pid = fork()) < 0) {           // fork error, i.e. too many processes
-        err(0, "fork error\n"); // will fprintf the error and go on
-        return EXIT_FAILURE;
-    }
+            sigaction(SIGINT, NULL, &oldact);   // the  previous action for SIGINT is saved in oldact
+            newact = oldact;
 
-    else if (pid == 0) {                 // child process
+            if ((pid = fork()) < 0) {           // fork error, i.e. too many processes
+                err(0, "fork error\n"); // will fprintf the error and go on
+                return EXIT_FAILURE;
+            }
 
-        // ============== NEW CODE HERE ==============
-        newact.sa_handler = SIG_DFL;
-        sigaction(SIGINT, &newact, NULL);   // reset SIGINT default for child
+            else if (pid == 0) {                 // child process
 
-        printf("execvp\n");
-        execvp(argv[0], argv);    // if successful, child will go on with new executable
-        err(EXIT_FAILURE, "execvp error\n"); // if exec not successful, just exit child
-        exit(EXIT_SUCCESS);
-    }
+                // ============== NEW CODE HERE ==============
+                newact.sa_handler = SIG_DFL;
+                sigaction(SIGINT, &newact, NULL);   // reset SIGINT default for child
 
-    else {                               // pid > 0: parent (shell) process
+                execvp(argv[0], argv);    // if successful, child will go on with new executable
+                err(EXIT_FAILURE, "execvp error\n"); // if exec not successful, just exit child
+                exit(EXIT_SUCCESS);
+            }
 
-        // ============== NEW CODE HERE ==============
-        newact.sa_handler = SIG_IGN;
-        sigaction(SIGINT, &newact, NULL);   // ignore SIGINT while waiting
+            else {                               // pid > 0: parent (shell) process
 
-        waitpid(pid, &wait_status, 0);
+                // ============== NEW CODE HERE ==============
+                newact.sa_handler = SIG_IGN;
+                sigaction(SIGINT, &newact, NULL);   // ignore SIGINT while waiting
 
-        // ============== NEW CODE HERE ==============
-        sigaction(SIGINT, &oldact, NULL);   // restore SIGINT when child finishes
+                waitpid(pid, &wait_status, 0);
 
-        fprintf(stderr, "Ended sleep child process\n");
+                // ============== NEW CODE HERE ==============
+                sigaction(SIGINT, &oldact, NULL);   // restore SIGINT when child finishes
 
-        // do something with wait_status
-        return WEXITSTATUS(wait_status);
-    }
+                fprintf(stderr, "Ended sleep child process\n");
+
+                // do something with wait_status
+                return WEXITSTATUS(wait_status);
+            }
 }
