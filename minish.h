75398ab355301457bfede8cc9ffdcb2c0d8fdbc1
lucasjmelogno-montevideolabs <81168112+sentinelia@users.noreply.github.com>
2022-06-07 21:24:48 -0300

    Minor changes

--- minish.h
+++ minish.h
@@ -3,6 +3,7 @@
 #define MAXWORDS 256        // cantidad máxima de palabras en la línea
 #define HISTORY_FILE	".minish_history"   // nombre del archivo que almacena historia de comandos
 
+
 // Definición de Estructuras
 
 struct builtin_struct {         // struct con información de los builtins
@@ -11,12 +12,25 @@ struct builtin_struct {         // struct con información de los builtins
     char *help_txt;             // el texto de ayuda
 };
 
+#define HELP_CD      "cd [..|dir] - cambia de directorio corriente"
+#define HELP_DIR     "dir [str]- muestra archivos en directorio corriente, que tengan 'str'"
+#define HELP_EXIT    "exit [status] - finaliza el minish con un status de retorno (por defecto 0)"
+#define HELP_HELP    "help [cd|dir|exit|help|history|getenv|pid|setenv|status|uid]"
+#define HELP_HISTORY "history [N] - muestra los últimos N (10) comandos escritos"
+#define HELP_GETENV  "getenv var [var] - muestra valor de variable(s) de ambiente"
+#define HELP_PID     "pid - muestra Process Id del minish"
+#define HELP_SETENV  "setenv var valor - agrega o cambia valor de variable de ambiente"
+#define HELP_STATUS  "status - muestra status de retorno de ultimo comando ejecutado"
+#define HELP_UID     "uid - muestra nombre y número de usuario dueño del minish"
+
 
 // Variables que deben definirse en el main como externas
 
 extern int globalstatret;	// guarda status del ultimo comando
 
-extern struct builtin_struct builtin_arr[];
+extern struct builtin_struct builtin_arr[] = {
+    {"cd", builtin_cd, HELP_CD} //hacer esto para todos los comandos
+};
 
 /*
     builtin_arr es una lista de los builtins, que se recorrerá en forma lineal.
@@ -40,18 +54,8 @@ extern struct builtin_struct builtin_arr[];
     La siguiente lista de constantes de texto puede adaptarse/mejorarse, se pone como ejemplo.
     Lo lógico sería que estuvieran definidas en el mismo fuente que define el array builtin_arr
     en lugar de estar en este archivo .h.
-
-#define HELP_CD      "cd [..|dir] - cambia de directorio corriente"
-#define HELP_DIR     "dir [str]- muestra archivos en directorio corriente, que tengan 'str'"
-#define HELP_EXIT    "exit [status] - finaliza el minish con un status de retorno (por defecto 0)"
-#define HELP_HELP    "help [cd|dir|exit|help|history|getenv|pid|setenv|status|uid]"
-#define HELP_HISTORY "history [N] - muestra los últimos N (10) comandos escritos"
-#define HELP_GETENV  "getenv var [var] - muestra valor de variable(s) de ambiente"
-#define HELP_PID     "pid - muestra Process Id del minish"
-#define HELP_SETENV  "setenv var valor - agrega o cambia valor de variable de ambiente"
-#define HELP_STATUS  "status - muestra status de retorno de ultimo comando ejecutado"
-#define HELP_UID     "uid - muestra nombre y número de usuario dueño del minish"
 */
+
 
 // Funciones a definir en los correspondientes archivos fuentes
 
