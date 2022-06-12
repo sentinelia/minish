e764327b28acfea30e4e218dbd4ae4a2e39978a6
sentinelia <lmelogno@correo.um.edu.uy>
2022-06-12 15:35:43 -0300

    fixed linea2argv

--- linea2argv.c
+++ linea2argv.c
@@ -3,33 +3,27 @@
 #include <stdlib.h>
 #include "minish.h"
 
-int wordcount(char linea[]){
+
+int linea2argv (char *linea, int argc, char **argv){
+    int k=0;
     int csum = 0; //VARIABLE BOOLEANA INDICA SI DEBE SUMAR AL CONTADOR DE PALABRAS
     int wrdlength = 0;
+
     for(int i=0; linea[i]!='\0';i++){
         if((linea[i]==' '||linea[i]=='\n') && csum==1){ //SI PUEDO CONTAR Y ENCUENTRO ESPACIO O SALTO DE LINEA, SUMO
+            argv[wrdlength][k+1]='\0';
             wrdlength++;
+            k=0;
             csum = 0;
+            
         } else if(linea[i]!=' ' && linea[i]!='\n'){ //SI VUELVO A ENCONTRAR UN CARACTER PUEDO VOLVER A SUMAR
             csum=1;
+            argv[wrdlength][k]=linea[i];
+            k++;
+            
         }
     }
     return wrdlength;
-}
-
-int linea2argv (char *linea, int argc, char **argv){
-    int strlength=strlen(linea);
-    int j=0;
-    int k=0;
-    for(int i=0;i<strlength-1;i++){
-        if(linea[i]==' '||linea[i]=='\t'){
-            j++;
-            k=0;
-        }else{
-            argv[j][k++]=linea[i];
-        }
-    }
-    return j;
     
 }
 
